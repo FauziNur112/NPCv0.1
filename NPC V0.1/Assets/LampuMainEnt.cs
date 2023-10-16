afd39689561ff780d483aafcbe0a092f06a15a50
Fanur <105720266+FauziNur112@users.noreply.github.com>
2023-10-16 22:01:58 +0700

    Klik Lampu On Off = E

--- NPC V0.1/Assets/LampuMainEnt.cs
+++ NPC V0.1/Assets/LampuMainEnt.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class LampuMainEnt : MonoBehaviour
 {
 
     public GameObject[] lampuMainent;
+    private GameObject lampu;
+    private bool Onoff = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +20,17 @@ public class LampuMainEnt : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            /*lampuMainent.setActive;*/
+            if (Onoff)
+            {
+                lampuMainent[0].SetActive(false);
+                Onoff = false;
+            } else
+            {
+                lampuMainent[0].SetActive(true);
+                Onoff = true;  
+         
+            }
+      
         }
     }
 }
