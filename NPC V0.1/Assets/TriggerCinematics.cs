1977c9e573af4ccb4d2e91678d68aaa3503dad8c
Fanur <105720266+FauziNur112@users.noreply.github.com>
2023-12-14 15:18:43 +0700

    Updating

--- NPC V0.1/Assets/TriggerCinematics.cs
+++ NPC V0.1/Assets/TriggerCinematics.cs
@@ -6,11 +6,13 @@ using UnityEngine.Playables;
 public class TriggerCinematics : MonoBehaviour
 {
     public PlayableDirector Act;
+    public GameObject skenariosatu;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            Act.Play();
+            /*Act.Play();*/
+            skenariosatu.GetComponent<TriggerDialog>().StartDialog(); 
             gameObject.GetComponent<BoxCollider2D>().enabled = false;
         }
     }
